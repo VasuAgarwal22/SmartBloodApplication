c31eec04cac3846541390c511f27c716549f18c0
VasuAgarwal22 <avasu769@gmail.com>
2026-01-20 20:54:55 +0530

    Second commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      pages: path.resolve(__dirname, './src/pages'),
+      contexts: path.resolve(__dirname, './src/contexts'),
+      lib: path.resolve(__dirname, './src/lib'),
+      styles: path.resolve(__dirname, './src/styles'),
+      utils: path.resolve(__dirname, './src/utils'),
+    },
+  },
+})
